41ba3c8d16c948c1c77d6d057ad5817e53ce4409
Glaucio Breno <glauciobreno@gmail.com>
2023-01-17 02:12:18 -0300

    subindo projeto e configs iniciais

---
+++ src/pages/Store.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { BsFillCartCheckFill, BsFillCartPlusFill } from 'react-icons/bs'
+import { getItem, setItem } from "../services/LocalStoreFuncs";
+
+export const Store = () => {
+
+    const [data, setData] = useState([]);
+    const [cart, setCart] = useState(getItem('carrinhoyt') || []);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+          const url = 'https://api.mercadolibre.com/sites/MLB/search?q=celular';
+          const response = await fetch(url);
+          const objJson = await response.json()
+          setData(objJson.results)
+        }
+        fetchApi();
+      },[])
+
+      const handleClick = (obj) => {
+        const element = cart.find((e) => e.id === obj.id)
+        if(element){
+            const arrFilter = cart.filter((e) => e.id !== obj.id)
+            setCart(arrFilter)
+            setItem('carrinhoyt', arrFilter)
+        } else {
+            setCart([...cart, obj])
+            setItem('carrinhoyt', [...cart, obj])
+        }
+      }
+
+    return (
+        <div>
+            <h1>store</h1>
+            <div>
+                {
+                    data.map((e) => (
+                        <div key={e.id}>
+                            <h4>{e.title}</h4>
+                            <img src={e.thumbnail} alt="" />
+                            <h4>{e.price}</h4>
+                            <button
+                            onClick={() => handleClick(e)}
+                            >
+                                {
+                                    cart.some((itemCart) => itemCart.id === e.id) ? (
+                                        <BsFillCartCheckFill/>
+                                    ) : (
+                                        <BsFillCartPlusFill />
+                                    )
+                                }
+                            </button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
